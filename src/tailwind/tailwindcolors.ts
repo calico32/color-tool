a9fb8cdb720193bd0518242028ab0ac6a8c81ed6
Caleb Chan <cchan@hey.com>
2025-06-09 10:48:09 -0400

    add typst formats

--- src/tailwind/tailwindcolors.ts
+++ src/tailwind/tailwindcolors.ts
@@ -71,6 +71,13 @@ const shades = [
 
 type ColorFormat = [(c: Color) => Color, (c: Color) => string]
 
+function formatTypstOklch(c: Color): string {
+  const color = oklch(c)
+  return `oklch(${(color.l * 100).toFixed(2)}%, ${color.c.toFixed(4)}, ${(
+    color.h ?? 0
+  ).toFixed(2)}deg)`
+}
+
 const copyFormats: Record<string, ColorFormat> = {
   Hex: [rgb, formatHex],
   Plain: [rgb, (c: Color) => formatHex(c).substring(1)],
@@ -84,6 +91,7 @@ const copyFormats: Record<string, ColorFormat> = {
   Oklch: [oklch, formatCss],
   Okhsl: [okhsl, formatCss],
   Okhsv: [okhsv, formatCss],
+  'Typst Oklch': [oklch, formatTypstOklch],
 }
 
 const copyFormatPreviews = Object.fromEntries(
@@ -99,6 +107,9 @@ const swatchCopyFormats = {
   CSV: '$value, ...',
   TSV: '$value\\t...',
   CSS: '--$color-$shade: $value',
+  'Typst Bindings': '#let $color-$shade = $value',
+  'Typst Dict': '( $key: $value )',
+  'Typst Array': '($value, ...)',
 } as const
 
 function colorContrast(bgColor: Color, fgColors: Color[]): Color {
@@ -139,6 +150,15 @@ function formatColor(inputColor: string, format: ColorFormat): string {
   return formatter(color)
 }
 
+// PickByValue<T, U> is the union of keys of T whose values are assignable to U.
+type PickByValue<T, U> = {
+  [K in keyof T]: T[K] extends U ? K : never
+}[keyof T]
+
+const swatches = Object.fromEntries(
+  Object.entries(colors).filter(([k, v]) => typeof v === 'object')
+) as Omit<typeof colors, PickByValue<typeof colors, string>>
+
 export default function (this: Magics<unknown>) {
   const handler = (ctx: any, event: MouseEvent) => {
     if (event.target === document.body) {
@@ -152,7 +172,7 @@ export default function (this: Magics<unknown>) {
   return {
     colors: colorNames,
     shades: shades,
-    swatches: colors,
+    swatches: swatches,
     copyFormats: copyFormats,
     copyFormatPreviews: copyFormatPreviews,
     swatchCopyFormats: swatchCopyFormats,
@@ -263,6 +283,111 @@ export default function (this: Magics<unknown>) {
       }, 1000)
     },
 
+    copyAllToClipboard(element: HTMLElement) {
+      const swatchFormat = this.selectedSwatchCopyFormat
+      const colors = Object.fromEntries(
+        Object.entries(this.swatches).map(([color, shades]) => [
+          color,
+          Object.fromEntries(
+            Object.entries(shades).map(([shade, value]) => [
+              shade,
+              formatColor(value, this.copyFormats[this.selectedCopyFormat]),
+            ])
+          ),
+        ])
+      )
+
+      switch (swatchFormat) {
+        case 'JSON': {
+          navigator.clipboard.writeText(JSON.stringify(colors, null, 2))
+          break
+        }
+        case 'Array': {
+          const array = Object.values(colors).map((shades) =>
+            Object.values(shades)
+          )
+          navigator.clipboard.writeText(JSON.stringify(array, null, 2))
+          break
+        }
+        case 'CSV': {
+          const header =
+            'shade,' + Object.keys(colors[Object.keys(colors)[0]]).join(',')
+          const values = Object.entries(colors)
+            .map(
+              ([color, shades]) => color + ',' + Object.values(shades).join(',')
+            )
+            .join('\n')
+          navigator.clipboard.writeText(header + '\n' + values)
+          break
+        }
+        case 'TSV': {
+          const header =
+            'color\t' + Object.keys(colors[Object.keys(colors)[0]]).join('\t')
+          const values = Object.entries(colors)
+            .map(
+              ([color, shades]) =>
+                color + '\t' + Object.values(shades).join('\t')
+            )
+            .join('\n')
+          navigator.clipboard.writeText(header + '\n' + values)
+          break
+        }
+        case 'CSS': {
+          const css = Object.entries(colors)
+            .flatMap(([color, shades]) =>
+              Object.entries(shades).map(
+                ([shade, value]) => `--${color}-${shade}: ${value};`
+              )
+            )
+            .join('\n')
+          navigator.clipboard.writeText(css)
+          break
+        }
+        case 'Typst Bindings': {
+          const typstBindings = Object.entries(colors)
+            .flatMap(([color, shades]) =>
+              Object.entries(shades).map(
+                ([shade, value]) => `#let ${color}-${shade} = ${value};`
+              )
+            )
+            .join('\n')
+          navigator.clipboard.writeText(typstBindings)
+          break
+        }
+        case 'Typst Dict': {
+          const dicts = Object.entries(colors)
+            .map(
+              ([color, shades]) =>
+                `#let ${color} = (\n` +
+                Object.entries(shades)
+                  .map(([shade, value]) => `  s${shade}: ${value},`)
+                  .join('\n') +
+                `\n);`
+            )
+            .join('\n')
+
+          navigator.clipboard.writeText(dicts)
+          break
+        }
+        case 'Typst Array': {
+          const typstArray = Object.values(colors).flatMap((shades) =>
+            Object.values(shades)
+          )
+          navigator.clipboard.writeText(`(${typstArray})`)
+          break
+        }
+        default: {
+          throw new Error(`Unknown swatch format: ${swatchFormat}`)
+        }
+      }
+
+      const originalContents = element.innerHTML
+      element.innerHTML = '☑️'
+      setTimeout(() => {
+        element.innerHTML = originalContents
+      }, 1000)
+    },
+
     copyColorSwatchToClipboard(
       element: HTMLElement,
       color: (typeof colorNames)[number]
@@ -300,6 +425,28 @@ export default function (this: Magics<unknown>) {
             .map(([k, v]) => `--${color}-${k}: ${v};`)
             .join('\n')
           navigator.clipboard.writeText(css)
+          break
+        }
+        case 'Typst Bindings': {
+          const typstBindings = colors
+            .map(([k, v]) => `#let ${color}-${k} = ${v};`)
+            .join('\n')
+          navigator.clipboard.writeText(typstBindings)
+          break
+        }
+        case 'Typst Dict': {
+          const typstDict = colors
+            .map(([k, v]) =>
+              /[0-9]/.test(k[0]) ? `  s${k}: ${v},` : `  ${k}: ${v},`
+            )
+            .join('\n')
+
+          navigator.clipboard.writeText(`(\n${typstDict}\n)`)
+          break
+        }
+        case 'Typst Array': {
+          const typstArray = colors.map(([k, v]) => v).join(', ')
+          navigator.clipboard.writeText(`(${typstArray})`)
           break
         }
         default: {
@@ -360,6 +507,23 @@ export default function (this: Magics<unknown>) {
             .map(([k, v]) => `--${k}-${shade}: ${v};`)
             .join('\n')
           navigator.clipboard.writeText(css)
+          break
+        }
+        case 'Typst Bindings': {
+          const typstBindings = colors
+            .map(([k, v]) => `#let ${k}-${shade} = ${v};`)
+            .join('\n')
+          navigator.clipboard.writeText(typstBindings)
+          break
+        }
+        case 'Typst Dict': {
+          const typstDict = colors.map(([k, v]) => `  ${k}: ${v},`).join('\n')
+          navigator.clipboard.writeText(`(\n${typstDict}\n)`)
+          break
+        }
+        case 'Typst Array': {
+          const typstArray = colors.map(([k, v]) => v).join(', ')
+          navigator.clipboard.writeText(`(${typstArray})`)
           break
         }
         default: {
